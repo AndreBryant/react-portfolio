187449d9c20effafa6a206e4830eb87518d88487
embryzon <95128949+embryzon@users.noreply.github.com>
2025-06-24 23:05:56 +0800

    Feat: Add about me initial components

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,30 +1,90 @@
-export default function About() {
-  return (
-    <section className="relative flex h-screen flex-col py-32" id="about">
-      <h2>ABOUT</h2>
-      {/* Education */}
-      <div className="">
-        <h2>Education</h2> {/**Change these to h3 */}
-        <ul>
-          <li>UPB (2021 - 2025)</li>
-          <li>Calamba Science (2019 - 2021)</li>
-          <li>DOST SEI JLSS Scholar (2023 - 2025)</li>
-        </ul>
-      </div>
+type LI = {
+  title: string;
+  duration: string;
+  desc: string[];
+};
 
-      {/* Organizations and Experience*/}
-      <div className="">
-        <h2>Organizations / Experience</h2>
-        <ul>
-          <li>
-            COMSCI@UP.BAG Research Committee Resident Member (2022 - 2025)
-          </li>
-          <li>
-            Software Developer Intern at DOST (June - August 2024) - Under the
-            Project LODI
-          </li>
-        </ul>
+export default function About() {
+  const educationListContent: LI[] = [
+    {
+      title: "University of the Philippines",
+      duration: "2021-2025",
+      desc: ["Bachelor of Science in Computer Science", "Cum Laude"],
+    },
+    {
+      title: "Calamba City Science Integrated School",
+      duration: "2019-2021",
+      desc: ["STEM", "With Honors"],
+    },
+    {
+      title: "DOST-SEI Scholar",
+      duration: "2023-2025",
+      desc: ["JLSS 2023 Awardee"],
+    },
+  ];
+
+  const experienceListContent: LI[] = [
+    {
+      title: "COMSCI@UP.BAG Resident Member",
+      duration: "2022-2025",
+      desc: ["Research Committee member"],
+    },
+    {
+      title: "DOST Internship",
+      duration: "June - August 2024",
+      desc: [
+        "Software Developer Intern under the Project LODI",
+        "completed 240 hours",
+      ],
+    },
+  ];
+
+  return (
+    <section className="relative flex h-screen flex-col gap-8 py-32" id="about">
+      <h2 className="text-4xl">About me</h2>
+
+      <div className="flex flex-col gap-8 border px-8 py-4 lg:flex-row">
+        <div className="flex flex-1/2 flex-col gap-4">
+          <UnorderedList list={educationListContent} header="Education" />
+        </div>
+
+        <div className="flex flex-1/2 flex-col gap-4">
+          <UnorderedList
+            list={experienceListContent}
+            header="Organizations / Experience"
+          />
+        </div>
       </div>
     </section>
+  );
+}
+
+function ListItem({ li }: { li: LI }) {
+  return (
+    <li>
+      <div>
+        <p>
+          {li.title} <span>({li.duration})</span>
+        </p>
+        <ul>
+          {li.desc.map((desc, index) => (
+            <li key={index}>{desc}</li>
+          ))}
+        </ul>
+      </div>
+    </li>
+  );
+}
+
+function UnorderedList({ list, header }: { list: LI[]; header: string }) {
+  return (
+    <>
+      <h3 className="text-xl">{header}</h3>
+      <ul>
+        {list.map((li, index) => (
+          <ListItem key={index} li={li} />
+        ))}
+      </ul>
+    </>
   );
 }
